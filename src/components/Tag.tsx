e7f24b39dc04a94eaeb142636b81b20b2f7ca5f5
snehamoybag <102692389+snehamoybag@users.noreply.github.com>
2025-11-01 19:28:10 +0530

    refractor: make tags name lowercase

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -13,7 +13,7 @@ export default function Tag({
   return (
     <Link
       to={`/tags/${name}`}
-      className={`block text-base text-neutral-200 capitalize no-underline px-2 pb-0.5 border-1 border-current rounded-full opacity-50 hover:opacity-100 ${className}`}
+      className={`block text-base text-neutral-200 lowercase no-underline px-2 pb-0.5 border-1 border-current rounded-full opacity-50 hover:opacity-100 ${className}`}
     >
       <span className="opacity-50">#</span> {name}
     </Link>
